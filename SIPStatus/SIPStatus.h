ecae56e1b419ac046a9ae50c84e76c41999a1002
npyl <n.pylarinos@hotmail.com>
2018-09-26 16:00:21 +0300

    Initial Commit

---
+++ SIPStatus/SIPStatus.h
@@ -0,0 +1,19 @@
+//
+//  SIPStatus.h
+//  SIPStatus
+//
+//  Created by Nickolas Pylarinos Stamatelatos on 26/09/2018.
+//  Copyright © 2018 Nickolas Pylarinos Stamatelatos. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SIPStatus.
+FOUNDATION_EXPORT double SIPStatusVersionNumber;
+
+//! Project version string for SIPStatus.
+FOUNDATION_EXPORT const unsigned char SIPStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SIPStatus/PublicHeader.h>
+
+
